5824ef2bcedcde489411f27cf1c0429b72a13de2
Philip <p.gazagnes@vacanceselect.com>
2023-06-07 15:36:09 +0200

    setup : vue use / typescript / vant 4

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <div>
+      <van-button type="primary">Primary</van-button>
+      <van-button type="success">Success</van-button>
+      <van-button type="default">Default</van-button>
+      <van-button type="danger">Danger</van-button>
+      <van-button type="warning">Warning</van-button>
+      {{ x }} - {{ y }}
+    </div>
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const { x, y } = useMouse()
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  typescript: { typeCheck: true },
+  modules: ['@vant/nuxt', '@vueuse/nuxt'],
 })
